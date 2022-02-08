8d8ab412e24936b28357a3f31064f9ed4f2ed3b0
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2022-02-08 16:11:03 +0000

    Add new version number

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LSDtopotools/gpm_precipitation_tools',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
