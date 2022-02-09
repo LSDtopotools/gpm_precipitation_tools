019632f0f3a4fc82cf8234248020dc319829db1e
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2022-02-09 16:38:51 +0000

    update requirements file to try and fix conflicts

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LSDtopotools/gpm_precipitation_tools',
-    version='0.4.3',
+    version='0.4.4',
     zip_safe=False,
 )
