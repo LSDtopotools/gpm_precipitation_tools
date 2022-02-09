20b72d8fac161cd7686706e198fc501d1d07fd7a
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2022-02-09 15:27:58 +0000

    add PyQt5

--- gpm_precipitation_tools/__init__.py
+++ gpm_precipitation_tools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Marina Ruiz Sanchez-Oro"""
 __email__ = 'marina.ruiz.so@ed.ac.uk'
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 __license__ = 'MIT'
 
 # from .PPT_CMD_RUN import *
