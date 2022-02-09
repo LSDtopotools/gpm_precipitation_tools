cbe13343ffba1b1086ad1eb34ef4a9f5585eefe6
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2022-02-09 15:46:49 +0000

    update pyqt and gdal versions

--- gpm_precipitation_tools/__init__.py
+++ gpm_precipitation_tools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Marina Ruiz Sanchez-Oro"""
 __email__ = 'marina.ruiz.so@ed.ac.uk'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __license__ = 'MIT'
 
 # from .PPT_CMD_RUN import *
